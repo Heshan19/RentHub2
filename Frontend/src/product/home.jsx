b9cfdc0e9412777995bfa358b848febc32f79b24
Himantha Amarathunga <himanthaa@gmail.com>
2023-01-31 23:13:05 +0530

    Home page

--- Frontend/src/product/home.jsx
+++ Frontend/src/product/home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "../Navbar/Navbar";
 import { Link } from "react-router-dom";
+import "./home.css";
 
 export default class home extends Component {
   render() {
@@ -8,18 +9,18 @@ export default class home extends Component {
       <div>
         <Navbar />
         <div className="home-middle-container">
-          <p>
-            Elevate your construction game with top-quality equipment rentals!
+          <p className="top-paragraph">
+            Elevate your construction game <br/> with top-quality equipment rentals!
           </p>
           <div className="row">
             <div className="input-field col s12">
-              <button type="submit">
+              <button type="submit" className="renthub-btn">
                 <Link to="/productList" className="nav-links ">
                   <i className="fas fa-home" />
                   Browse all machines
                 </Link>
               </button>
-              <button type="submit">
+              <button type="submit" className="renthub-btn">
                 <Link to="/productList" className="nav-links ">
                   <i className="fas fa-home" />
                   Rent with rent hub
@@ -27,11 +28,21 @@ export default class home extends Component {
               </button>
             </div>
           </div>
-          <div>
-            <p>The Rent hub Experience</p>
-            <p>Construction equipment rentals offer a cost-effective and convenient solution for any project. Rather than purchasing expensive tools and machinery, you can easily rent the equipment you need for the duration of your project. This saves you money on upfront costs and maintenance fees. Plus, with a wide variety of equipment available, you'll have access to the latest and most advanced tools in the industry. Construction equipment rentals make it easy to get the job done efficiently and effectively.</p>
-          </div>
+          
         </div>
+        <div className="row">
+        <div className="input-field col s12">
+            <p className="sub-heading">The Rent hub Experience</p>
+            </div>
+            <div className="input-field col s12">
+            <p className="bottom-paragraph">Construction equipment rentals offer a cost-effective and convenient solution for any <br/>
+                project. Rather than purchasing expensive tools and machinery, you can easily rent <br/>
+                the equipment you need for the duration of your project. This saves you money on <br/>
+                upfront costs and maintenance fees. Plus, with a wide variety of equipment available, <br/>
+                you'll have access to the latest and most advanced tools in the industry. Construction <br/>
+                equipment rentals make it easy to get the job done efficiently and effectively.</p>
+           </div>
+          </div>
       </div>
     );
   }
